86335afa05b20e6baf5b3c89f5df1d305a53e165
Kurhade, Vedant Sunil <vedant.kurhade@bhge.com>
2020-08-25 16:21:40 +0530

    Added Calculation helper

---
+++ Helper/CalculationHelper.cs
@@ -0,0 +1,20 @@
+﻿namespace Calculator.Helper
+{
+    public class CalculationHelper
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public string Add(string a, string b)
+        {
+            return a + b;
+        }
+    }
+}
