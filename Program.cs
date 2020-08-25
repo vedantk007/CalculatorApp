6709a9ee1dba810ae8fb095c81b625c935d25bf1
Kurhade, Vedant Sunil <vedant.kurhade@bhge.com>
2020-08-25 10:05:50 +0530

    Add integers functionality added

--- Program.cs
+++ Program.cs
@@ -2,11 +2,16 @@
 
 namespace Calculator
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
         }
     }
 }
